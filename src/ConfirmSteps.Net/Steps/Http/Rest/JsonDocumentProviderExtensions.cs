d77724a6255e89d54c56904e48a5784b58bcecd6
Grégory Célet <1457422+gcelet@users.noreply.github.com>
2023-08-15 22:59:06 +0200

    :sparkles: feat: add jsonpath array selector methods

--- src/ConfirmSteps.Net/Steps/Http/Rest/JsonDocumentProviderExtensions.cs
+++ src/ConfirmSteps.Net/Steps/Http/Rest/JsonDocumentProviderExtensions.cs
@@ -13,21 +13,30 @@ public static class JsonDocumentProviderExtensions
             return null;
         }
 
-        switch (jsonElement.Value.ValueKind)
+        return jsonElement.Value.ValueKind switch
         {
-            case JsonValueKind.True:
-            {
-                return true;
-            }
-            case JsonValueKind.False:
-            {
-                return false;
-            }
-            default:
-            {
-                return null;
-            }
+            JsonValueKind.True => true,
+            JsonValueKind.False => false,
+            JsonValueKind.Null => null,
+            _ => throw new ArgumentException()
+        };
+    }
+
+    public static bool?[]? SelectBooleanArray(this IJsonDocumentProvider jsonDocumentProvider, string jsonPath)
+    {
+        if (!jsonDocumentProvider.SelectJsonElements(jsonPath, null, out IList<JsonElement>? jsonElements))
+        {
+            return null;
         }
+
+        return jsonElements?.Where(e => e is { ValueKind: JsonValueKind.True or JsonValueKind.False or JsonValueKind.Null })
+            .Select(e => e.ValueKind switch
+            {
+                JsonValueKind.True => true,
+                JsonValueKind.False => false,
+                JsonValueKind.Null => (bool?)null,
+                _ => throw new ArgumentException()
+            }).ToArray();
     }
 
     public static decimal? SelectNumber(this IJsonDocumentProvider jsonDocumentProvider, string jsonPath)
@@ -41,6 +50,23 @@ public static class JsonDocumentProviderExtensions
         return jsonElement.Value.GetDecimal();
     }
 
+    public static decimal?[]? SelectNumberArray(this IJsonDocumentProvider jsonDocumentProvider, string jsonPath)
+    {
+        if (!jsonDocumentProvider.SelectJsonElements(jsonPath, null,
+                out IList<JsonElement>? jsonElements))
+        {
+            return null;
+        }
+
+        return jsonElements?.Where(e => e is { ValueKind: JsonValueKind.Number or JsonValueKind.Null })
+            .Select(e => e.ValueKind switch
+            {
+                JsonValueKind.Number => e.GetDecimal(),
+                JsonValueKind.Null => (decimal?)null,
+                _ => throw new ArgumentException()
+            }).ToArray();
+    }
+
     public static string? SelectString(this IJsonDocumentProvider jsonDocumentProvider, string jsonPath)
     {
         if (!jsonDocumentProvider.SelectJsonElement(jsonPath, JsonValueKind.String, out JsonElement? jsonElement) ||
@@ -52,31 +78,61 @@ public static class JsonDocumentProviderExtensions
         return jsonElement.Value.GetString();
     }
 
+    public static string?[]? SelectStringArray(this IJsonDocumentProvider jsonDocumentProvider, string jsonPath)
+    {
+        if (!jsonDocumentProvider.SelectJsonElements(jsonPath, JsonValueKind.String,
+                out IList<JsonElement>? jsonElements))
+        {
+            return null;
+        }
+
+        return jsonElements?.Select(e => e.GetString()).ToArray();
+    }
+
     private static bool SelectJsonElement(this IJsonDocumentProvider jsonDocumentProvider,
         string jsonPath, JsonValueKind? jsonValueKind, out JsonElement? jsonElement)
     {
         jsonElement = null;
+
+        if (!jsonDocumentProvider.SelectJsonElements(jsonPath, jsonValueKind,
+                out IList<JsonElement>? localJsonElements))
+        {
+            return false;
+        }
+
+        if (localJsonElements is not { Count: 1 })
+        {
+            return false;
+        }
+
+        jsonElement = localJsonElements[0];
+        return true;
+    }
+
+    private static bool SelectJsonElements(this IJsonDocumentProvider jsonDocumentProvider,
+        string jsonPath, JsonValueKind? jsonValueKind, out IList<JsonElement>? jsonElements)
+    {
+        jsonElements = null;
 
         if (jsonDocumentProvider.JsonDocument == null)
         {
             return false;
         }
 
-        IList<JsonElement> jsonElements = JsonSelector.Select(jsonDocumentProvider.JsonDocument.RootElement, jsonPath);
+        IList<JsonElement> localJsonElements =
+            JsonSelector.Select(jsonDocumentProvider.JsonDocument.RootElement, jsonPath);
 
-        if (jsonElements.Count != 1)
+        if (localJsonElements.Count == 0)
         {
             return false;
         }
 
-        JsonElement localJsonElement = jsonElements[0];
-
-        if (jsonValueKind.HasValue && localJsonElement.ValueKind != jsonValueKind.Value)
+        if (jsonValueKind.HasValue && localJsonElements.Any(e => e.ValueKind != jsonValueKind.Value))
         {
             return false;
         }
 
-        jsonElement = localJsonElement;
+        jsonElements = localJsonElements;
         return true;
     }
 }
---
+++ tests/ConfirmSteps.Net.Tests/Http/Extensions.cs
@@ -0,0 +1,68 @@
+﻿namespace ConfirmSteps.Net.Tests.Http;
+
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using ConfirmSteps.Steps.Http.Problems;
+using ConfirmSteps.Steps.Http.Rest;
+
+public static class Extensions
+{
+    public static HttpResponseJson ToHttpResponseJson(this JsonObject jsonObject,
+        HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+    {
+        JsonDocument? jsonDocument = jsonObject.Deserialize<JsonDocument>();
+        HttpResponseMessage httpResponseMessage = jsonObject.ToHttpResponseMessage(httpStatusCode);
+        HttpResponseJson httpResponseJson = new(httpResponseMessage, jsonDocument);
+
+        return httpResponseJson;
+    }
+
+    public static RestApiResult ToRestApiResult(this JsonObject jsonObject,
+        HttpStatusCode httpStatusCode = HttpStatusCode.OK,
+        bool isProblem = false)
+    {
+        HttpResponseJson httpResponseJson = jsonObject.ToHttpResponseJson(httpStatusCode);
+        JsonDocument? jsonDocument = jsonObject.Deserialize<JsonDocument>();
+        RestApiResult restApiResult = new(httpResponseJson,
+            isProblem ? null : jsonDocument,
+            isProblem ? jsonDocument : null);
+
+        return restApiResult;
+    }
+
+    public static RestApiResult<T> ToRestApiResult<T>(this JsonObject jsonObject,
+        HttpStatusCode httpStatusCode = HttpStatusCode.OK,
+        bool isProblem = false)
+        where T : class
+    {
+        HttpResponseJson httpResponseJson = jsonObject.ToHttpResponseJson(httpStatusCode);
+        JsonDocument? jsonDocument = jsonObject.Deserialize<JsonDocument>();
+        T? result = isProblem
+            ? null
+            : jsonDocument?.Deserialize<T>();
+        ProblemDetails? problemDetails = isProblem
+            ? jsonDocument?.Deserialize<ProblemDetails>()
+            : null;
+        RestApiResult<T> restApiResult = new(httpResponseJson,
+            isProblem ? null : jsonDocument,
+            isProblem ? jsonDocument : null,
+            result, problemDetails);
+
+        return restApiResult;
+    }
+
+    private static HttpResponseMessage ToHttpResponseMessage(this JsonObject jsonObject,
+        HttpStatusCode httpStatusCode)
+    {
+        string json = jsonObject.ToJsonString();
+        HttpResponseMessage httpResponseMessage = new(httpStatusCode)
+        {
+            Content = new StringContent(json, Encoding.UTF8, MediaTypeNames.Application.Json)
+        };
+
+        return httpResponseMessage;
+    }
+}
