78d014cc6cba2071bd5d0a6a020d262879cb2eb5
Grégory Célet <1457422+gcelet@users.noreply.github.com>
2026-03-29 18:58:08 +0200

    :sparkles: feat: add support for dotnet 8 and 10

---
+++ build/VersionDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class VersionDetails
+{
+  public static VersionDetails BuildDefaultFallbackVersion()
+  {
+    const string version = "0.1.0";
+    // const string preReleaseLabel = "ci";
+    const string preReleaseLabel = "";
+    // const string revision = "00001";
+    const string revision = "";
+
+    string revisionWithDot = !string.IsNullOrEmpty(revision)
+      ? $".{revision}"
+      : string.Empty;
+    string preReleaseTag = !string.IsNullOrEmpty(preReleaseLabel) && !string.IsNullOrEmpty(revisionWithDot)
+      ? $"{preReleaseLabel}{revisionWithDot}"
+      : string.Empty;
+    string preReleaseTagWithDash = !string.IsNullOrEmpty(preReleaseTag)
+      ? $"-{preReleaseTag}"
+      : string.Empty;
+
+    return new VersionDetails
+    {
+      PackageVersionPrefix = version,
+      PackageVersionSuffix = preReleaseTag,
+      Version = $"{version}{preReleaseTagWithDash}{revisionWithDot}",
+      AssemblyVersion = $"{version}{revisionWithDot}",
+      FileVersion = $"{version}{revisionWithDot}",
+      InformationalVersion = $"{version}{preReleaseTagWithDash}"
+    };
+  }
+
+  public required string AssemblyVersion { get; init; }
+
+  public required string FileVersion { get; init; }
+
+  public required string InformationalVersion { get; init; }
+
+  public required string PackageVersionPrefix { get; init; }
+
+  public required string PackageVersionSuffix { get; init; }
+
+  public required string Version { get; init; }
+}
